1bc0caeda61ea7471fea1c02defc7c217d55e015
Martín Nieva <martinjafactor@gmail.com>
2020-06-14 20:26:20 -0300

    Added more options

--- main.py
+++ main.py
@@ -2,19 +2,44 @@ import click
 import requests
 from bs4 import BeautifulSoup
 
+def slugify(string):
+    result = string.lower().replace(' ', '-')
+
+    return result
+
 def convert_international(price: str) -> int:
     price = int(price.replace('.', ''))
 
     return price
 
-response = requests.get('https://listado.mercadolibre.com.ar/glenlivet-founder-reserve')
+@click.command()
+@click.option('-u', '--get-url', 'get_url', flag_value=True, default=False)
+@click.option('-p', '--prices-number', 'prices_number', type=int, default=10)
+@click.argument('string_search', type=str)
+def main(string_search, get_url, prices_number):
+    url = 'https://listado.mercadolibre.com.ar/'
+    search = slugify(string_search)
 
-parsed = BeautifulSoup(response.text, 'html.parser')
+    complete_url = url + search
 
-price_tags = parsed.findAll('span', attrs={ 'class' : 'price__fraction' })
+    # glenlivet-founder-reserve
+    response = requests.get(complete_url)
 
-# Getting raw prices (int values)
-prices = [ convert_international(price.getText(strip=True, )) for price in price_tags ]
-prices.sort(reverse=True)
+    parsed = BeautifulSoup(response.text, 'html.parser')
 
-print(prices)
+    price_tags = parsed.findAll('span', attrs={ 'class' : 'price__fraction' })
+
+    # Getting raw prices (int values)
+    prices = [ convert_international(price.getText(strip=True, )) for price in price_tags ][:prices_number]
+    prices.sort(reverse=True)
+
+    minimum = prices[-1]
+    maximum = prices[0]
+
+    print(f'Max price: {maximum}\nMin price: {minimum}')
+
+    if get_url:
+        print(complete_url)
+
+if __name__ == '__main__':
+    main()
